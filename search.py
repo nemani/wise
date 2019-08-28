ae217c43c63a777dd042d0bde21eed77911495d9
Arjun Nemani <nemaniarjun@gmail.com>
2019-08-28 21:43:10 +0530

    Add search.py template given to us

---
+++ search.py
@@ -0,0 +1,37 @@
+import sys
+
+
+def read_file(testfile):
+    with open(testfile, 'r') as file:
+        queries = file.readlines()
+    return queries
+
+
+def write_file(outputs, path_to_output):
+    '''outputs should be a list of lists.
+        len(outputs) = number of queries
+        Each element in outputs should be a list of titles corresponding to a particular query.'''
+    with open(path_to_output, 'w') as file:
+        for output in outputs:
+            for line in output:
+                file.write(line.strip() + '\n')
+            file.write('\n')
+
+
+def search(path_to_index, queries):
+    '''Write your code here'''
+    pass
+
+
+def main():
+    path_to_index = sys.argv[1]
+    testfile = sys.argv[2]
+    path_to_output = sys.argv[3]
+
+    queries = read_file(testfile)
+    outputs = search(path_to_index, queries)
+    write_file(outputs, path_to_output)
+
+
+if __name__ == '__main__':
+    main()
